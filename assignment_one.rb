75d0d677f10a10ed505edbc3e0ede7d7e26d29c4
William DeFreese <alexdefreese@gmail.com>
2013-11-12 23:13:02 -0500

    Added stubs for assignment one

---
+++ assignment_one.rb
@@ -0,0 +1,12 @@
+module AssignmentOne
+  # Output a comma-separated list of 0..number-1
+  def count(number)
+
+  end
+
+  # Output "Invalid" if number is not a single-digit number
+  # otherwise output the name-represenation of the number
+  def num_to_name(number)
+
+  end
+end
